24a6924e575ced1f6d037a7e178e289dda325396
Dzung Nguyen <dzungng89@gmail.com>
2013-09-16 18:20:56 -0500

    Baum Welch  training

--- hmm.py
+++ hmm.py
@@ -23,7 +23,7 @@ class HMM:
             observations[t] = drawFrom(self.B[states[t],:])
         return observations,states
 
-    def forwardbackward(self, observations):
+    def evaluate(self, observations):
         """ Likelihood of a sequence
             Forward backward procedure
         """
@@ -31,10 +31,9 @@ class HMM:
 
         for i in range(1,len(observations)):
             alpha=np.dot(alpha,self.A)*self.B[:,observations[i]]
-            print alpha
         return sum(alpha)
 
-    def findstate(self, observations):
+    def decoding(self, observations):
         """ Find hidden state of HMM from observations
         Viterbi algorithm
         """
@@ -49,21 +48,91 @@ class HMM:
             # print np.log(delta)/np.log(2)
         return states
 
-    def hmmestimate(self):
+    def display(self):
+        # print self.A
+        # print self.B
+        print self.pi
+
+    def train(self, observations, criterion):
         """ Baum Welch estimation
         """
+        nStates = self.A.shape[0]
+        nSamples = len(observations)
+        converge=False
+        counter=0
+        while not converge and counter<10:
+            counter+=1
+            # ==================== Expectation step ======================
+            #scale factors
+            c = np.zeros(nSamples) 
 
- 
+            # Forward variable
+            alpha=np.zeros((nStates, nSamples))
+            alpha[:,0] = self.pi*self.B[:,observations[0]]
+            c[0] = 1.0/np.sum(alpha[:,0])
+            alpha[:,0] = c[0] * alpha[:,0]
+            for t in range(1,nSamples):
+                alpha[:,t] = np.dot(alpha[:, t-1],self.A)*self.B[:,observations[t]]
+                c[t] = 1.0/np.sum(alpha[:,t])
+                alpha[:,t] = c[t] * alpha[:,t]
+
+            # Backward variable
+            beta = np.zeros((nStates,nSamples))
+            beta[:,nSamples-1] = 1
+            beta[:,nSamples-1] = c[nSamples-1] * beta[:,nSamples-1]
+            for t in range(len(observations)-1,0,-1):
+                beta[:,t-1] = np.dot(self.A, (self.B[:,observations[t]] * beta[:,t]))
+                beta[:,t-1] = c[t-1] * beta[:,t-1]
+            
+            # Xi and gamma variables
+            xi = np.zeros((nSamples-1,nStates,nStates))
+            for t in range(nSamples-1):
+                denom = np.dot(np.dot(alpha[:,t].T, self.A) * self.B[:,observations[t+1]].T, beta[:,t+1])
+                for i in range(nStates):
+                    for j in range(nStates):
+                        numer = alpha[i,t] * self.A[i,j] * self.B[j,observations[t+1]] * beta[j,t+1]
+                        xi[t, i,j] = numer / denom
+
+            gamma=np.sum(xi,axis=2)
+            prod =  (alpha[:,nSamples-1] * beta[:,nSamples-1]).reshape((-1,1)).T[0]
+            gamma = np.vstack((gamma,  prod / np.sum(prod)))
+
+
+            # ================ Maximization step ========================
+            # Update HMM model parameters
+            newpi = gamma[0,:]
+            newA = np.sum(xi, axis=0)/ np.sum(gamma, axis=0).T
+            
+            newB = B
+            nLevels = self.B.shape[1]
+            sumgamma = np.sum(gamma,axis=0)
+            
+            for lev in range(nLevels):
+                mask = observations == lev
+                newB[:,lev] = np.sum(gamma[mask,:],axis=0) / sumgamma
+            
+            # Check if converge
+            if np.max(abs(pi - newpi)) < criterion and \
+                   np.max(abs(A - newA)) < criterion and \
+                   np.max(abs(B - newB)) < criterion:
+                converge=True
+  
+            self.pi=newpi
+            self.A=newA
+            self.B=newB
+
+            HMM.display(self)
 
 if __name__ == '__main__':
     #'Two states, three possible observations in a state'
-    pi = np.array([0.25, 0.5, 0.25])
-    A = np.array([[0.25, 0.5, 0.25],
-                      [0.25, 0.25, 0.5],
-                      [0.5, 0.5, 0]])
-    B = np.array([[1,0,0,0],
-                  [0.25,0.5,0,0.25],
-                  [0.25, 0.25, 0.25, 0.25]])
+    pi = np.array([0.5, 0.5])
+    A = np.array([[0.7, 0.3],
+                    [0.4, 0.6]])
+    B = np.array([[0.1, 0.4, 0.5],
+                  [0.7, 0.2, 0.1]])
 
     hmm=HMM(A,B,pi)
-    print hmm.findstate(np.array([1,1,2]))
+    obs=np.array([0,1,0,2,1,1,1])
+    hmm.train(obs,0.1)
+    
+
